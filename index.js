ce49043c4bd982152695c9eba77a89d8f63b3aeb
Evgeny <puls2007@rambler.ru>
2021-11-03 14:24:24 +0500

    подготовка к дз3
    # Conflicts:
    #	index.js

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-const colors = require("colors");
-const {validateArgs} = require("./hendleError");
+//const lesson1 = require("./lesson1");
+//const lesson2 = require("./lesson2");
+const lesson3 = require("./lesson3");
 
-const [, , one, two] = [...process.argv];
-
-try {
-    let [start, end] = validateArgs(one, two);
-    const colorList = [colors.green, colors.yellow, colors.red];
-
-    const result = [];
-
-    next: for (start; start <= end; start++) {
-        if (!(start % 2)) continue;
-        for (let i = 3; i < start; i++) {
-            if (!(start % i)) continue next;
-        }
-        result.push(start);
-    }
-
-    if (result.length) {
-        result.forEach((item, index) => {
-            const color = index % 3;
-            console.log(colorList[color](item));
-        })
-    } else {
-        console.log('Простые числа не найдены'.red)
-    }
-} catch (e) {
-    console.log(e.message.red);
-}
+//lesson1();
+//lesson2();
+lesson3();
