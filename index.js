94a5d70cab42532e467e604976d54bad880cf8d8
Evgeny <puls2007@rambler.ru>
2021-11-07 13:57:49 +0500

    дубль. Разбить по директориям домашние задания.

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-const colors = require("colors");
-const {validateArgs} = require("./hendleError");
+//const lesson1 = require("./lesson1");
+//const lesson2 = require("./lesson2");
+const lesson4 = require("./lesson4");
 
-const [, , one, two] = [...process.argv];
-
-try {
-    let [start, end] = validateArgs(one, two);
-    const colorList = [colors.green, colors.yellow, colors.red];
-
-    const result = [];
-
-    next: for (start; start <= end; start++) {
-        if (!(start % 2)) continue;
-        for (let i = 3; i < start; i++) {
-            if (!(start % i)) continue next;
-        }
-        result.push(start);
-    }
-
-    if (result.length) {
-        result.forEach((item, index) => {
-            const color = index % 3;
-            console.log(colorList[color](item));
-        })
-    } else {
-        console.log('Простые числа не найдены'.red)
-    }
-} catch (e) {
-    console.log(e.message.red);
-}
+lesson4();
